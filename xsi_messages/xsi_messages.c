b6312a92fde01ae4c9a33f03f6f792307088498e
石子龙 <zilongshi@gmail.com>
2017-12-02 11:19:23 +0800

    [Feature] xsi_messages.c : Add IPC XSI Messages source

---
+++ xsi_messages/xsi_messages.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include "../sys/debug.h"
+
+#define MSGKEY 1
+
+typedef struct
+{
+    int type;
+    char buf[512];
+} MSGBUF;
+
+void parent()
+{
+    int msgid;
+    int ret;
+    MSGBUF msg;
+
+    msgid = msgget(MSGKEY, IPC_CREAT | IPC_EXCL | 0666);
+    if (msgid < 0)
+    {
+        perror("msgget: ");
+        msgid = msgget(MSGKEY,0666);
+        goto remove;
+    }
+    usleep(100);
+    msg.type = 0;
+    strcpy(msg.buf, "I am parent.");
+
+    ret = msgsnd(msgid, &msg, sizeof(msg), IPC_NOWAIT);
+    if (ret < 0)
+    {
+        perror("msgsnd: ");
+        goto remove;
+    }
+
+remove:
+    ret = msgctl(msgid, IPC_RMID, NULL);
+    if (ret < 0)
+    {
+        perror("msgctl: ");
+    }
+}
+
+void child()
+{
+    int msgid;
+    int ret;
+    MSGBUF msg;
+
+    msgid = msgget(MSGKEY, 0666);
+    if (msgid < 0)
+    {
+        perror("msgget: ");
+    }
+    ret = msgrcv(msgid, &msg, sizeof(msg), 0, 0);
+
+    if (ret < 0)
+    {
+        perror("msgrcv: ");
+        return;
+    }
+
+    debug_info("msg is: %s\n", msg.buf);
+}
+
+int main(int argc, char const *argv[])
+{
+    pid_t pid;
+    int status;
+
+    if ((pid = fork()) < 0)
+    {
+        debug_error("Fork error");
+        exit(-1);
+    }
+    else if (pid > 0)
+    {
+        parent();
+        wait(&status);
+    }
+    else
+    {
+        child();
+
+    }
+    exit(0);
+}
