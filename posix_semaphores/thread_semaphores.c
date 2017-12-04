c2df40c5415d4aca4a1a28fac8f83b2bff3fe0e3
石子龙 <zilongshi@gmail.com>
2017-12-04 12:01:58 +0800

    [Feature] thread_semaphores : Add IPC Posix thread semaphore source

--- posix_semaphores/thread_semaphores.c
+++ posix_semaphores/thread_semaphores.c
@@ -1,6 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include "../sys/debug.h"
 
+sem_t sem;
+
+void *thread(void *dummy)
+{
+    sleep(1);
+    debug_info("new thread posting\n");
+    sem_post(&sem);
+
+    return NULL;
+}
 
 int main(int argc, char const *argv[])
 {
+    pthread_t pid;
+    pthread_create(&pid, NULL, thread, NULL);
+    sem_init(&sem, 0, 0);
 
+    sem_wait(&sem);
+    debug_info("main thread wait succeed\n");
+    sem_post(&sem);
+
+    sem_destroy(&sem);
+
+    pthread_join(pid, NULL);
+
+    return 0;
 }
